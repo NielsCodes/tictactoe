6092f08e2e09d309f636dd92874af93cbc5cd768
Niels Kersic <hi@niels.codes>
2021-10-03 15:08:08 +0200

    Add winning line representation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,12 @@ export class AppComponent {
     ['', '', ''],
   ];
 
+  winningBoard = [
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
+  ];
+
   scores: {[key: string]: number} = {
     X: -1,
     O: 1,
@@ -23,6 +29,11 @@ export class AppComponent {
   player: 'X' |'O' = 'X';
   winner: string | null = null;
 
+  /**
+   * Add the current player's character to the board at the specified position
+   * @param y the y coordinate to play (0-2)
+   * @param x the x coordinate to play (0-2)
+   */
   playSpace(y: number, x: number) {
     this.board[y][x] = this.player;
     if (this.player === 'X') {
@@ -31,37 +42,72 @@ export class AppComponent {
     } else {
       this.player = 'X';
     }
-    this.winner = this.checkWinner(this.board);
+    const [winner, winningBoard] = this.checkWinner(this.board);
+    this.winner = winner;
+    if (winner !== null && winner !== 'tie') {
+      this.winningBoard = winningBoard!;
+    }
   }
 
+  /** Use the minimax algorithm to find the optimal move for the AI player
+   * - Uses playSpace to make the move on the board
+   */
   moveAi() {
     const [score, position] = this.minimax(this.board, true);
-    console.log(score, position);
     if (position) {
       this.playSpace(position.i, position.j);
     }
   }
 
-  checkWinner(board: string[][]) {
+  /**
+   * Check if game representation has reached an end state
+   * - Returns null if game has not yet ended
+   * - Returns 'tie' if game has reached a tie
+   * - Returns the character of a winner and a representation of the winning line if game is won
+   * @param board a 2D array representation of the board
+   * @returns the winner and winning line representation
+   */
+  checkWinner(board: string[][]): [string | null, boolean[][] | null] {
     let openSpaces = 0;
 
-    if (
-      this.isFullLine(board[0][0], board[1][1], board[2][2]) ||
-      this.isFullLine(board[2][0], board[1][1], board[0][2])
-    ) {
-      return board[1][1];
+    const winningBoard = [
+      [false, false, false],
+      [false, false, false],
+      [false, false, false],
+    ];
+
+    // Check diagonals
+    if (this.isFullLine(board[0][0], board[1][1], board[2][2])) {
+      winningBoard[0][0] = true;
+      winningBoard[1][1] = true;
+      winningBoard[2][2] = true;
+      return [board[1][1], winningBoard];
+    }
+    if (this.isFullLine(board[2][0], board[1][1], board[0][2])) {
+      winningBoard[2][0] = true;
+      winningBoard[1][1] = true;
+      winningBoard[0][2] = true;
+      return [board[1][1], winningBoard];
     }
 
+    // Check rows
     for (let i=0; i < 3; i++) {
 
       if (this.isFullLine(board[i][0], board[i][1], board[i][2])) {
-        return board[i][0];
+        winningBoard[i][0] = true;
+        winningBoard[i][1] = true;
+        winningBoard[i][2] = true;
+        return [board[i][0], winningBoard];
       }
 
       if (this.isFullLine(board[0][i], board[1][i], board[2][i])) {
-        return board[0][i];
+        winningBoard[0][i] = true;
+        winningBoard[1][i] = true;
+        winningBoard[2][i] = true;
+        return [board[0][i], winningBoard];
       }
 
+      // Count open spaces
       for (let j=0; j < 3; j++) {
         if (board[i][j] === '') openSpaces++;
       }
@@ -69,10 +115,10 @@ export class AppComponent {
     }
 
     if (openSpaces === 0) {
-      return 'tie';
+      return ['tie', null];
     }
 
-    return null;
+    return [null, null];
   }
 
   /**
@@ -94,7 +140,7 @@ export class AppComponent {
    */
   minimax(board: string[][], isMaximizing: boolean): [number, {i: number, j: number} | null] {
     // Recursive base case
-    const winner = this.checkWinner(board);
+    const [winner] = this.checkWinner(board);
     if (winner !== null) {
       return [this.scores[winner], null];
     }
@@ -149,6 +195,11 @@ export class AppComponent {
       ['', '', ''],
       ['', '', ''],
       ['', '', ''],
+    ];
+    this.winningBoard = [
+      [false, false, false],
+      [false, false, false],
+      [false, false, false],
     ];
   }
 
